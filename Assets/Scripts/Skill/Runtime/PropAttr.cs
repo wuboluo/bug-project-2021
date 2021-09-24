75fb775860520983e09b5b550ed4f0fe319281f5
wuboluo <383049891@qq.com>
2021-09-24 19:05:50 +0800

    attr value type from float to int

--- Assets/Scripts/Skill/Runtime/PropAttr.cs
+++ Assets/Scripts/Skill/Runtime/PropAttr.cs
@@ -8,8 +8,6 @@ namespace Bug.Project21.Props
     [Serializable]
     public partial class PropAttr
     {
-        // ------------------------------------------------------------ 
-
         [ShowIf("isWeapon")] [Toggle("enable")] [LabelText("攻击")] [SerializeField]
         private Attr_Atk atk;
 
@@ -25,12 +23,19 @@ namespace Bug.Project21.Props
         // ------------------------------------------------------------ 
         [HideInInspector] public bool isWeapon;
 
+        /// <summary>
+        ///     判断道具属性的值是否都被设置过
+        /// </summary>
+        /// <returns>true：全部被设置</returns>
         public bool InitValues()
         {
             var values = new[] {atk.Value, def.Value, hp.Value, speed.Value};
             return !values.All(_ => _.Equals(default));
         }
 
+        /// <summary>
+        ///     设置道具属性的数值（范围随机）
+        /// </summary>
         public void SetValues()
         {
             atk.InitValue();
@@ -39,16 +44,27 @@ namespace Bug.Project21.Props
             speed.InitValue();
         }
 
+        /// <summary>
+        ///     获取道具属性的值
+        /// </summary>
+        /// <returns></returns>
         public (float _atk, float _def, float _hp, float _speed) GetValues()
         {
             return (atk.Value, def.Value, hp.Value, speed.Value);
+        }
+
+        /// <summary>
+        ///     获取道具属性是否可使用
+        /// </summary>
+        /// <returns></returns>
+        public (bool _atkAble, bool _defAble, bool _hpAble, bool _speedAble) GetAbles()
+        {
+            return (atk.enable, def.enable, hp.enable, speed.enable);
         }
     }
 
     public partial class PropAttr
     {
-        // ------------------------------------------------------------ 
-
         [HideIf("isWeapon")] [Toggle("enable")] [LabelText("价格")] [SerializeField]
         private Attr_Price price;
 
