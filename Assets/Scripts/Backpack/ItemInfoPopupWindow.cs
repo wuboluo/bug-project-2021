94185976a1fceeae99e909e172511bd17519c625
346832804@qq.com <346832804@qq.com>
2021-09-24 14:44:01 +0800

    333

--- Assets/Scripts/Backpack/ItemInfoPopupWindow.cs
+++ Assets/Scripts/Backpack/ItemInfoPopupWindow.cs
@@ -39,38 +39,38 @@ namespace Bug.Project21.Backpack
         public GameObject MyMoveSpeed = null;
         public void OnOpenWindow(Item item)
         {
-            foreach (Transform g in Group)
-            {
-                g.gameObject.SetActive(false);
-            }
-            MyName.text = item.DATA.Name;
-            MyIcon.texture = item.DATA.Icon;
-            MyIntroduce.text = item.DATA.Describe;
-            if (item.DATA.Attr.First().Atk > 0)
-            {
-                MyAtk.SetActive(true);
-                MyAtk.transform.GetChild(0).GetComponent<Text>().text = "攻击";
-                MyAtk.transform.GetChild(1).GetComponent<Text>().text = item.DATA.Attr.First().Atk.ToString();
-            }
-            if (item.DATA.Attr.First().Def > 0)
-            {
-                MyDef.SetActive(true);
-                MyDef.transform.GetChild(0).GetComponent<Text>().text = "防御";
-                MyDef.transform.GetChild(1).GetComponent<Text>().text = item.DATA.Attr.First().Def.ToString();
-            }
-            if (item.DATA.Attr.First().Hp > 0)
-            {
-                MyHp.SetActive(true);
-                MyHp.transform.GetChild(0).GetComponent<Text>().text = "生命";
-                MyHp.transform.GetChild(1).GetComponent<Text>().text = item.DATA.Attr.First().Hp.ToString();
-            }
-            if (item.DATA.Attr.First().Speed > 0)
-            {
-                MyMoveSpeed.SetActive(true);
-                MyMoveSpeed.transform.GetChild(0).GetComponent<Text>().text = "移动速度";
-                MyMoveSpeed.transform.GetChild(1).GetComponent<Text>().text = item.DATA.Attr.First().Speed.ToString();
-            }
-            this.transform.localScale = Vector3.one;
+            //foreach (Transform g in Group)
+            //{
+            //    g.gameObject.SetActive(false);
+            //}
+            //MyName.text = item.DATA.Name;
+            //MyIcon.texture = item.DATA.Icon;
+            //MyIntroduce.text = item.DATA.Describe;
+            //if (item.DATA.Attr.First().Atk > 0)
+            //{
+            //    MyAtk.SetActive(true);
+            //    MyAtk.transform.GetChild(0).GetComponent<Text>().text = "攻击";
+            //    MyAtk.transform.GetChild(1).GetComponent<Text>().text = item.DATA.Attr.First().Atk.ToString();
+            //}
+            //if (item.DATA.Attr.First().Def > 0)
+            //{
+            //    MyDef.SetActive(true);
+            //    MyDef.transform.GetChild(0).GetComponent<Text>().text = "防御";
+            //    MyDef.transform.GetChild(1).GetComponent<Text>().text = item.DATA.Attr.First().Def.ToString();
+            //}
+            //if (item.DATA.Attr.First().Hp > 0)
+            //{
+            //    MyHp.SetActive(true);
+            //    MyHp.transform.GetChild(0).GetComponent<Text>().text = "生命";
+            //    MyHp.transform.GetChild(1).GetComponent<Text>().text = item.DATA.Attr.First().Hp.ToString();
+            //}
+            //if (item.DATA.Attr.First().Speed > 0)
+            //{
+            //    MyMoveSpeed.SetActive(true);
+            //    MyMoveSpeed.transform.GetChild(0).GetComponent<Text>().text = "移动速度";
+            //    MyMoveSpeed.transform.GetChild(1).GetComponent<Text>().text = item.DATA.Attr.First().Speed.ToString();
+            //}
+            //this.transform.localScale = Vector3.one;
         }
 
         public void OnCloseWindow()
